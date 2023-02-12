597a5b2711297a1df4fed7c388b865d3c9467704
Evgeniy <evgeniyklimow@gmail.com>
2023-02-12 17:17:54 +0300

    add 3 projects dz

---
+++ zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i = 1; i <= b; i++)
+    {
+    result *= a;
+    }
+Console.WriteLine($"Результат: {result}");
---
+++ zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+while (number != 0)
+{
+sum += number % 10;
+number /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе: {sum}");
